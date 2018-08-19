46b98a5bae8f71d984d04e2ed943fb0c8bbffc22
Vincent Vella <vincevella0@gmail.com>
2018-08-19 02:29:33 -0400

    Implements Redux

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import './assets/css/animate.min.css';
+import './assets/css/demo.css';
+import './assets/css/pe-icon-7-stroke.css';
+import './assets/sass/light-bootstrap-dashboard.css';
+import orgs from './redux/orgs';
+import indexRoutes from './routes';
 
-import { HashRouter, Route, Switch } from "react-router-dom";
-
-import indexRoutes from "./routes/index";
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/css/animate.min.css";
-import "./assets/sass/light-bootstrap-dashboard.css?v=1.2.0";
-import "./assets/css/demo.css";
-import "./assets/css/pe-icon-7-stroke.css";
+const store = createStore(
+  combineReducers([orgs]),
+  {},
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 ReactDOM.render(
-  <HashRouter>
-    <Switch>
-      {indexRoutes.map((prop, key) => {
-        return <Route to={prop.path} component={prop.component} key={key} />;
-      })}
-    </Switch>
-  </HashRouter>,
-  document.getElementById("root")
+  <Provider store={store}>
+    <HashRouter>
+      <Switch>
+        {indexRoutes.map(
+          (prop, key) => <Route to={prop.path} component={prop.component} key={key} />,
+        )}
+      </Switch>
+    </HashRouter>
+  </Provider>,
+  document.getElementById('root'),
 );
---
+++ src/redux/orgs.js
@@ -0,0 +1,10 @@
+const initialState = {
+  orgs: {},
+};
+
+export default function orgs(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
