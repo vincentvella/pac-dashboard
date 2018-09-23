5fe01027e5cd5966b520fe7b4515aff41b27419d
Vincent Vella <vincevella0@gmail.com>
2018-09-23 14:19:07 -0400

    ESLint cleaning

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase/app';
+
 require('firebase/database');
 require('firebase/auth');
 
@@ -6,20 +7,20 @@ export let ref;
 export let storageRef;
 
 export const setUpFirebase = () => {
-	let config = {
-		apiKey: "AIzaSyAurCabmoak3wLR4TkvTvvMW-urlW5tqbQ",
-		authDomain: "pacalendar.firebaseapp.com",
-		databaseURL: "https://pacalendar.firebaseio.com/",
-		projectId: "pacalendar",
-		storageBucket: "pac-event-app.appspot.com",
-		messagingSenderId: "751145285044"
-	};
+  const config = {
+    apiKey: 'AIzaSyAurCabmoak3wLR4TkvTvvMW-urlW5tqbQ',
+    authDomain: 'pacalendar.firebaseapp.com',
+    databaseURL: 'https://pacalendar.firebaseio.com/',
+    projectId: 'pacalendar',
+    storageBucket: 'pac-event-app.appspot.com',
+    messagingSenderId: '751145285044',
+  };
 
-	firebase.initializeApp(config);
+  firebase.initializeApp(config);
 
-	let db = firebase.database();
-	let bucket = firebase.storage().ref('images');
+  const db = firebase.database();
+  const bucket = firebase.storage().ref('images');
 
-	ref = db.ref();
-	storageRef = bucket;
+  ref = db.ref();
+  storageRef = bucket;
 };
